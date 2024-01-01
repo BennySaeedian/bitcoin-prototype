572067e0425744706f3c80425ab1e359c6e91caa
benyamin <benyaminsa@gmail.com>
2024-01-01 14:20:02 +0200

    small changes

--- cryptographic_utils.py
+++ cryptographic_utils.py
@@ -16,10 +16,10 @@ from custom_typing import PrivateKey, Signature, PublicKey
 
 def sign(message: bytes, private_key: PrivateKey) -> Signature:
     """signs the given message using the given private key"""
-    pk = Ed25519PrivateKey.from_private_bytes(
+    pk_object = Ed25519PrivateKey.from_private_bytes(
         private_key
     )
-    return Signature(pk.sign(message))
+    return Signature(pk_object.sign(message))
 
 
 def verify(message: bytes, signature: Signature, public_key: PublicKey) -> bool:
@@ -27,9 +27,9 @@ def verify(message: bytes, signature: Signature, public_key: PublicKey) -> bool:
     verifies a signature for a given message using a public key
     returns true iff the signature matches
     """
-    pub_k = Ed25519PublicKey.from_public_bytes(data=public_key)
+    candidate = Ed25519PublicKey.from_public_bytes(data=public_key)
     try:
-        pub_k.verify(signature=signature, data=message)
+        candidate.verify(signature=signature, data=message)
         return True
     except:
         return False
@@ -41,16 +41,16 @@ def generate_keys() -> tuple[PrivateKey, PublicKey]:
     The keys are returned in bytes format to allow them to be serialized easily
     """
     private_key = Ed25519PrivateKey.generate()
-    priv_key_bytes = private_key.private_bytes(
+    private_key_bytes = private_key.private_bytes(
         encoding=Encoding.Raw,
         format=PrivateFormat.Raw,
         encryption_algorithm=NoEncryption(),
     )
-    pub_key_bytes = private_key.public_key().public_bytes(
+    public_key_bytes = private_key.public_key().public_bytes(
         encoding=Encoding.Raw,
         format=PublicFormat.Raw,
     )
-    return PrivateKey(priv_key_bytes), PublicKey(pub_key_bytes)
+    return PrivateKey(private_key_bytes), PublicKey(public_key_bytes)
 
 
 def crypto_hash(input_: bytes) -> bytes:
