7dd84f0e9ba8e7f4bdb6072e1386f1c15717d0b9
benyamin <benyaminsa@gmail.com>
2024-04-10 11:58:44 +0300

    fix constants namespace bug

--- src/block.py
+++ src/block.py
@@ -40,3 +40,4 @@ class Block:
     def get_prev_block_hash(self) -> BlockHash:
         """Gets the hash of the previous block"""
         return self.prev_block_hash
+    
--- tests/test_basic_cases.py
+++ tests/test_basic_cases.py
@@ -8,23 +8,23 @@ def test_node_at_init(alice: Node) -> None:
     assert alice.get_utxo() == []
     assert alice.get_mempool() == []
     assert alice.get_balance() == 0
-    assert alice.get_latest_hash() == Constants.GENESIS_BLOCK_PREV
+    assert alice.get_latest_hash() == GENESIS_BLOCK_PREV
     alice_public_key = alice.get_address()
     assert alice.create_transaction(alice_public_key) is None
 
 
 def test_block_mining(alice: Node) -> None:
     block_hash = alice.mine_block()
-    assert block_hash != Constants.GENESIS_BLOCK_PREV
+    assert block_hash != GENESIS_BLOCK_PREV
     assert alice.get_latest_hash() == block_hash
-    assert alice.get_balance() == Constants.NUM_OF_COINBASE_PER_BLOCK
-    assert len(alice.get_utxo()) == Constants.NUM_OF_COINBASE_PER_BLOCK
+    assert alice.get_balance() == NUM_OF_COINBASE_PER_BLOCK
+    assert len(alice.get_utxo()) == NUM_OF_COINBASE_PER_BLOCK
     assert alice.get_mempool() == []
     block = alice.get_block(block_hash)
     assert block.get_hash() == block_hash
-    assert block.get_prev_block_hash() == Constants.GENESIS_BLOCK_PREV
+    assert block.get_prev_block_hash() == GENESIS_BLOCK_PREV
     transactions = block.get_transactions()
-    assert len(transactions) == Constants.NUM_OF_COINBASE_PER_BLOCK
+    assert len(transactions) == NUM_OF_COINBASE_PER_BLOCK
     transaction, *_ = transactions
     assert transaction.get_id() == alice.get_utxo()[0].get_id()
     assert transaction.is_coinbase
@@ -33,7 +33,7 @@ def test_block_mining(alice: Node) -> None:
 
 def test_block_retrieval(alice: Node) -> None:
     with pytest.raises(ValueError):
-        alice.get_block(Constants.GENESIS_BLOCK_PREV)
+        alice.get_block(GENESIS_BLOCK_PREV)
     junk_hash = BlockHash(b'beneath this mask there is an idea and ideas are bulletproof')
     with pytest.raises(ValueError):
         alice.get_block(junk_hash)
@@ -47,7 +47,7 @@ def test_connections(alice: Node, bob: Node, charlie: Node) -> None:
     alice.connect(bob)
     first_hash = alice.mine_block()
     assert {bob.get_latest_hash(), alice.get_latest_hash()} == {first_hash}
-    assert charlie.get_latest_hash() == Constants.GENESIS_BLOCK_PREV
+    assert charlie.get_latest_hash() == GENESIS_BLOCK_PREV
     second_hash = bob.mine_block()
     assert {bob.get_latest_hash(), alice.get_latest_hash()} == {second_hash}
     charlies_hash = charlie.mine_block()
@@ -57,7 +57,7 @@ def test_connections(alice: Node, bob: Node, charlie: Node) -> None:
 
 def test_moving_funds_and_balances(alice: Node, bob: Node) -> None:
     alice.mine_block()
-    assert alice.get_balance() == Constants.NUM_OF_COINBASE_PER_BLOCK
+    assert alice.get_balance() == NUM_OF_COINBASE_PER_BLOCK
     transaction = alice.create_transaction(bob.get_address())
     assert transaction is not None
     assert transaction.input == alice.get_utxo()[0].get_id()
@@ -67,12 +67,12 @@ def test_moving_funds_and_balances(alice: Node, bob: Node) -> None:
     bob.connect(alice)
     assert bob.get_balance() == 0
     bob.mine_block()
-    assert bob.get_balance() == Constants.NUM_OF_COINBASE_PER_BLOCK
+    assert bob.get_balance() == NUM_OF_COINBASE_PER_BLOCK
     assert bob.get_mempool() == []
     alice.mine_block()
     assert alice.get_mempool() == []
-    assert alice.get_balance() == Constants.NUM_OF_COINBASE_PER_BLOCK
-    assert bob.get_balance() == Constants.NUM_OF_COINBASE_PER_BLOCK + 1
+    assert alice.get_balance() == NUM_OF_COINBASE_PER_BLOCK
+    assert bob.get_balance() == NUM_OF_COINBASE_PER_BLOCK + 1
 
 
 def test_self_connections_fail(alice: Node) -> None:
